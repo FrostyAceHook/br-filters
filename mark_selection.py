8a46b4980f20bf0c1a980adf01780ea4171c7ee8
FrostyAceHook <69618630+FrostyAceHook@users.noreply.github.com>
2024-07-02 03:24:54 +1000

    mark selection uses the user selection
    - instead of just the two extreme corners.

--- mark_selection.py
+++ mark_selection.py
@@ -4,23 +4,33 @@ displayName = "Mark Selection"
 
 inputs = (
     ("Block:", alphaMaterials.Glowstone),
-    ("Replaces the most-positive corner and the most-negative corner of the "
-            "selection with the block above. Used to mark a selection so that "
-            "in the future, this selection can easily be made by clicking these "
-            "two blocks.", "label"),
+    ("Replaces the two corners of the selection with 'block'. Used to mark a "
+            "selection so that in the future, this selection can easily be made "
+            "by clicking these two blocks.", "label"),
 )
 
 
 def perform(level, box, options):
     bid, bdata = options["Block:"].ID, options["Block:"].blockData
 
-    # Most-negative corner.
-    level.setBlockAt(box.minx, box.miny, box.minz, bid)
-    level.setBlockDataAt(box.minx, box.miny, box.minz, bdata)
+    try:
+        # Try do a little dodgy and access the "leaked" editor object to set the
+        # genuine corners of the selection (i.e. the yellow and blue blocks).
+        x1, y1, z1 = editor.selectionTool.bottomLeftPoint
+        x2, y2, z2 = editor.selectionTool.topRightPoint
+    except Exception:
+        # Otherwise just take the two extreme corners.
+        x1, y1, z1 = box.origin
+        x2, y2, z2 = box.maximum
+        # The upper bound isn't included but we wanna place blocks in the
+        # selection.
+        x2, y2, z2 = x2 - 1, y2 - 1, z2 - 1
 
-    # Most-positive corner.
-    level.setBlockAt(box.maxx - 1, box.maxy - 1, box.maxz - 1, bid)
-    level.setBlockDataAt(box.maxx - 1, box.maxy - 1, box.maxz - 1, bdata)
+    # Set the two corners.
+    level.setBlockAt(x1, y1, z1, bid)
+    level.setBlockAt(x2, y2, z2, bid)
+    level.setBlockDataAt(x1, y1, z1, bdata)
+    level.setBlockDataAt(x2, y2, z2, bdata)
 
     print "Finished marking selection."
     return
